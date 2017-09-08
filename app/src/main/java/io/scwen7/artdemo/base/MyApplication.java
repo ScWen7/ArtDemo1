caa800f4c764c923e2b55dbdd62cad8e6036de75
ScWen7 <ab811733738@163.com>
2017-09-08 18:16:21 +0800

    Initial commit create project

---
+++ app/src/main/java/io/scwen7/artdemo/base/MyApplication.java
@@ -0,0 +1,16 @@
+package io.scwen7.artdemo.base;
+
+import android.app.Application;
+
+/**
+ * Created by 解晓辉 on 2017/9/8.
+ * 作用：
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
